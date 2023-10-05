0060be1ce5a6f390a3fed5a4949c045b2fa79fa9
Tama0026 <mtam7896@gmail.com>
2023-10-05 17:11:27 +0700

    Tama

---
+++ Group4.FacilitiesReport/Group4.FacilitiesReport.DAO/IUser.cs
@@ -0,0 +1,17 @@
+﻿using Group4.FacilitiesReport.DAO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group4.FacilitiesReport.Interface
+{
+    internal interface IUser
+    {
+        public bool ChangePassword(DTO.User user);
+        public bool UpdateInfo( DTO.User user);
+
+
+    }
+}
