c6bdd8c166e5659ffc1ed590adb2cd90908154f4
KanaeRudo <KanaeRudo@gmail.com>
2023-10-06 22:25:25 +0700

    Fixed Database

---
+++ Group4.FacilitiesReport/Repositories/FeedbackRepo.cs
@@ -0,0 +1,45 @@
+﻿using Group4.FacilitiesReport.DTO;
+using Group4.FacilitiesReport.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group4.FacilitiesReport.Repositories
+{
+    internal class FeedbackRepo : IFeedback
+    {
+        public DbContext Context { get;  } = 
+        public Feedback GetAllFeedBack()
+        {
+            Context.
+        }
+
+        public Feedback GetFeedbackByUserID(string UserID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool NotifyFeedback(string feedbackID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateFeedback(Feedback feedback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateFeedbackResponse(Feedback feedback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateFeedbackStatus(string feedbackID, int status)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
