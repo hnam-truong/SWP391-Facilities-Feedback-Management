5eb1ca083a99cd67dcdd89aee71fa1f87c9666fa
hnam-truong <truonghahaonam@gmail.com>
2023-10-17 16:06:52 +0700

    Sign In

--- src/layouts/authentication/sign-in/index.js
+++ src/layouts/authentication/sign-in/index.js
@@ -13,8 +13,8 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
-import { useState } from "react";
-
+import React, { useState } from "react";
+import { GoogleLogin } from "react-google-login";
 // react-router-dom components
 import { Link } from "react-router-dom";
 
@@ -23,11 +23,6 @@ import Card from "@mui/material/Card";
 import Switch from "@mui/material/Switch";
 import Grid from "@mui/material/Grid";
 import MuiLink from "@mui/material/Link";
-
-// @mui icons
-import FacebookIcon from "@mui/icons-material/Facebook";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import GoogleIcon from "@mui/icons-material/Google";
 
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
@@ -39,52 +34,45 @@ import MDButton from "components/MDButton";
 import BasicLayout from "layouts/authentication/components/BasicLayout";
 
 // Images
-import bgImage from "assets/images/bg-sign-in-basic.jpeg";
+// import bgImage from "assets/images/bg-sign-in-basic.jpeg";
+import bgImage from "assets/images/HCM-scaled.jpeg";
+import logo from "assets/images/logo-no-background.png";
+import google from "assets/images/google-icon-2048x2048-czn3g8x8.png"
 
 function Basic() {
+  const clientId = 'YOUR_CLIENT_ID';
   const [rememberMe, setRememberMe] = useState(false);
-
+  const [logoPosition, setLogoPosition] = useState({ mx: -1, mt: 4, p: -3, mb: 1 });
   const handleSetRememberMe = () => setRememberMe(!rememberMe);
+  const onSuccess = (response) => {
+    console.log('Login Success:', response);
+  };
+
+  const onFailure = (error) => {
+    console.log('Login Failed:', error);
+  };
 
   return (
     <BasicLayout image={bgImage}>
       <Card>
         <MDBox
-          variant="gradient"
-          bgColor="info"
-          borderRadius="lg"
-          coloredShadow="info"
-          mx={2}
-          mt={-3}
-          p={2}
-          mb={1}
+          // variant="gradient"
+          // bgColor="info"
+          // borderRadius="lg"
+          // coloredShadow="info"
+          // mx={1}
+          // mt={1}
+          // p={-3}
+          // mb={1}
           textAlign="center"
+          {...logoPosition}
         >
-          <MDTypography variant="h4" fontWeight="medium" color="white" mt={1}>
-            Sign in
-          </MDTypography>
-          <Grid container spacing={3} justifyContent="center" sx={{ mt: 1, mb: 2 }}>
-            <Grid item xs={2}>
-              <MDTypography component={MuiLink} href="#" variant="body1" color="white">
-                <FacebookIcon color="inherit" />
-              </MDTypography>
-            </Grid>
-            <Grid item xs={2}>
-              <MDTypography component={MuiLink} href="#" variant="body1" color="white">
-                <GitHubIcon color="inherit" />
-              </MDTypography>
-            </Grid>
-            <Grid item xs={2}>
-              <MDTypography component={MuiLink} href="#" variant="body1" color="white">
-                <GoogleIcon color="inherit" />
-              </MDTypography>
-            </Grid>
-          </Grid>
+          <img src={logo} alt="logo" style={{ width: "220px", height: "auto" }} />
         </MDBox>
         <MDBox pt={4} pb={3} px={3}>
           <MDBox component="form" role="form">
             <MDBox mb={2}>
-              <MDInput type="email" label="Email" fullWidth />
+              <MDInput type="FeID" label="FeID" fullWidth />
             </MDBox>
             <MDBox mb={2}>
               <MDInput type="password" label="Password" fullWidth />
@@ -102,24 +90,30 @@ function Basic() {
               </MDTypography>
             </MDBox>
             <MDBox mt={4} mb={1}>
-              <MDButton variant="gradient" color="info" fullWidth>
-                sign in
-              </MDButton>
-            </MDBox>
-            <MDBox mt={3} mb={1} textAlign="center">
-              <MDTypography variant="button" color="text">
-                Don&apos;t have an account?{" "}
-                <MDTypography
-                  component={Link}
-                  to="/authentication/sign-up"
-                  variant="button"
-                  color="info"
-                  fontWeight="medium"
-                  textGradient
-                >
-                  Sign up
-                </MDTypography>
-              </MDTypography>
+              <Link to="/dashboard">
+                <MDButton variant="gradient" color="info" fullWidth>
+                  sign in
+                </MDButton>
+              </Link>
+              {/* <MDButton variant="outline" style={{backgroundColor: '#5490F4'}} fullWidth> */}
+              <GoogleLogin
+                clientId="YOUR_GOOGLE_CLIENT_ID"
+                render={(renderProps) => (
+                  <button
+                    onClick={renderProps.onClick}
+                    disabled={renderProps.disabled}
+                    style={{ marginTop: '10px', width: '100%', height: '40px', borderStyle:'solid',borderColor:'#6e6e79', borderRadius: '10px', display: 'flex', alignItems: 'center', justifyContent: 'center', color: '#6e6e79', backgroundColor: '#ffff'}}
+                  >
+                    <img src={google} alt="google" style={{ width: '20px', height: 'auto', marginRight: '5px' }}></img>
+                    <strong>SIGN IN WITH GOOGLE</strong>
+                  </button>
+                )}
+                onSuccess={onSuccess}
+                onFailure={onFailure}
+                cookiePolicy={'single_host_origin'}
+              />
+
+              {/* </MDButton> */}
             </MDBox>
           </MDBox>
         </MDBox>
--- src/routes.js
+++ src/routes.js
@@ -43,22 +43,22 @@ const routes = [
     route: "/notifications",
     component: <Notifications />,
   },
-  {
-    type: "collapse",
-    name: "Profile",
-    key: "profile",
-    icon: <Icon fontSize="small">person</Icon>,
-    route: "/profile",
-    component: <Profile />,
-  },
   // {
   //   type: "collapse",
-  //   name: "Sign In",
-  //   key: "sign-in",
-  //   icon: <Icon fontSize="small">login</Icon>,
-  //   route: "/authentication/sign-in",
-  //   component: <SignIn />,
+  //   name: "Profile",
+  //   key: "profile",
+  //   icon: <Icon fontSize="small">person</Icon>,
+  //   route: "/profile",
+  //   component: <Profile />,
   // },
+    {
+      type: "collapse",
+      name: "Sign In",
+      key: "sign-in",
+      icon: <Icon fontSize="small">login</Icon>,
+      route: "/authentication/sign-in",
+      component: <SignIn />,
+    },
 ];
 
 export default routes;
