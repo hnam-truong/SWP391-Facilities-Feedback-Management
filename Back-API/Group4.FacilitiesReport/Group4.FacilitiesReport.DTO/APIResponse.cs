04ea889866afaad70e95ff7f078a4c9881d0cd10
hnam-truong <90917843+hnam-truong@users.noreply.github.com>
2023-11-15 11:30:12 +0700

    Delete Back-API directory

--- Back-API/Group4.FacilitiesReport/Group4.FacilitiesReport.DTO/APIResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Group4.FacilitiesReport.DTO
-{
-    public class APIResponse
-    {
-        public int ResponseCode { get; set; }
-        public string Result { get; set; }
-        public string ErrorMessage { get; set; }
-    }
-}
--- Back-API/Group4.FacilitiesReport/Group4.FacilitiesReport.DTO/ConfigDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Group4.FacilitiesReport.DTO
-{
-    public class ConfigDTO
-    {
-        public Guid Id { get; set; }
-        public string Variable { get; set; }
-        public string Value { get; set; }
-    }
-}
